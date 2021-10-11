573208e6cc2a80811cdf7b20cc99646b3568b53b
Jason Gessner <jason@multiply.org>
2021-10-10 23:09:47 -0500

    Add timestamps for rounds and pairings.

---
+++ db/migrate/20211003030446_add_updated_at_to_rounds_and_pairings.rb
@@ -0,0 +1,6 @@
+class AddUpdatedAtToRoundsAndPairings < ActiveRecord::Migration[5.2]
+  def change
+    add_timestamps :rounds, null: false, default: -> { 'NOW()' }
+    add_timestamps :pairings, null: false, default: -> { 'NOW()' }
+  end
+end
