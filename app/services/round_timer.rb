8f2b7a81e0e76e3a398826d5bfb06009343e3b6f
haradan <haradan@users.noreply.github.com>
2022-08-29 16:57:39 +0100

    Compute round timer finish time

--- app/services/round_timer.rb
+++ app/services/round_timer.rb
@@ -2,17 +2,22 @@ class RoundTimer
   attr_reader :round
   delegate :round_timer_activations, to: :round
   delegate :completed?, to: :round
+  delegate :length_minutes, to: :round
 
   def initialize(round)
     @round = round
   end
 
   def start!
-    round_timer_activations.create!
+    round_timer_activations.create! start_time: Time.current
   end
 
   def show?
     round_timer_activations.count > 0 && !completed?
+  end
+
+  def finish_time
+    round_timer_activations.last.start_time + length_minutes.minutes
   end
 
 end
--- spec/feature/rounds/round_timer_spec.rb
+++ spec/feature/rounds/round_timer_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'round timer' do
   let(:tournament) { create(:tournament) }
   let(:round) { create(:round, stage: tournament.current_stage) }
+  delegate :timer, to: :round
 
   context 'viewing tournament rounds as the tournament creator' do
     before do
@@ -20,5 +21,11 @@ RSpec.describe 'round timer' do
 
   it 'has a default round length' do
     expect(round.length_minutes).to be(65)
+  end
+
+  it 'computes finish time' do
+    travel_to Time.zone.local(2022, 8, 29, 15, 6)
+    timer.start!
+    expect(timer.finish_time).to eq Time.zone.local(2022, 8, 29, 16, 11)
   end
 end
