9c3b0fc6474244968d40abd94353ce01ea77082b
haradan <haradan@users.noreply.github.com>
2022-10-06 00:59:21 +0100

    Avoid su in wait-for-postgres

--- bin/postgresql-container-wait-for-postgres.sh
+++ bin/postgresql-container-wait-for-postgres.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 RETRIES=10
-su - postgres
 
-until psql -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until psql --username=postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 5
 done
