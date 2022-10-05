da65cbceb9bf3fd75948955f18c4c34b9cc26cf6
haradan <haradan@users.noreply.github.com>
2022-10-05 23:45:27 +0100

    Separate out wait-for-postgres script

--- bin/postgresql-upgrade-fns.sh
+++ bin/postgresql-upgrade-fns.sh
@@ -8,10 +8,12 @@ compose_db_upgrade() {
 postgresql_upgrade() {
   compose_db_upgrade rm -s -f db db-old
   compose_db_upgrade up -d db-old
+  compose_db_upgrade exec -T db-old bash < ${SCRIPTS_DIR}/postgresql-container-wait-for-postgres.sh
   compose_db_upgrade exec -T db-old bash < ${SCRIPTS_DIR}/postgresql-container-make-dump.sh
   compose_db_upgrade rm -s -f db-old
   compose_db_upgrade run --rm -T db-old bash < ${SCRIPTS_DIR}/postgresql-container-move-data.sh
   compose_db_upgrade up -d db
+  compose_db_upgrade exec -T db bash < ${SCRIPTS_DIR}/postgresql-container-wait-for-postgres.sh
   compose_db_upgrade exec -T db bash < ${SCRIPTS_DIR}/postgresql-container-restore-dump.sh
 }
 
