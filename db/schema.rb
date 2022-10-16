5b3bbf9a339fbdc591f6c464cee454ae74daa2f0
jhkelly6418 <jhkelly6418@gmail.com>
2022-10-16 08:06:41 -0400

    re-apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_203619) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_214640) do
->>>>>>> main
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_114319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
