547a97381396897283d1b425bf58647651e218bd
haradan <haradan@users.noreply.github.com>
2022-10-16 23:42:40 +0100

    Make bin/postgres/in-container folder

--- bin/postgres/postgresql-upgrade-fns.sh
+++ bin/postgres/postgresql-upgrade-fns.sh
@@ -1,5 +1,5 @@
 set -e
-SCRIPTS_DIR=$(cd $(dirname $0) && pwd)
+CONTAINER_SCRIPTS_DIR=$(cd $(dirname $0) && cd "in-container" && pwd)
 BASE_DIR=$(cd $(dirname $0) && cd "../../" && pwd)
 pushd ${BASE_DIR}
 
@@ -10,13 +10,13 @@ compose_db_upgrade_do() {
 compose_db_upgrade_exec() {
   SERVICE=$1
   SCRIPT_NAME=$2
-  compose_db_upgrade_do exec -T ${SERVICE} bash < ${SCRIPTS_DIR}/${SCRIPT_NAME}
+  compose_db_upgrade_do exec -T ${SERVICE} bash < ${CONTAINER_SCRIPTS_DIR}/${SCRIPT_NAME}
 }
 
 compose_db_upgrade_run() {
   SERVICE=$1
   SCRIPT_NAME=$2
-  compose_db_upgrade_do run --rm -T ${SERVICE} bash < ${SCRIPTS_DIR}/${SCRIPT_NAME}
+  compose_db_upgrade_do run --rm -T ${SERVICE} bash < ${CONTAINER_SCRIPTS_DIR}/${SCRIPT_NAME}
 }
 
 compose_db_upgrade_exec_with_wait() {
@@ -24,16 +24,16 @@ compose_db_upgrade_exec_with_wait() {
   DB_SCRIPT_NAME=$2
   compose_db_upgrade_do up -d ${DB_SERVICE}
   # Need to wait because docker-compose up returns before the database finishes starting
-  compose_db_upgrade_exec ${DB_SERVICE} postgresql-container-wait-for-postgres.sh
+  compose_db_upgrade_exec ${DB_SERVICE} wait-for-postgres.sh
   compose_db_upgrade_exec ${DB_SERVICE} ${DB_SCRIPT_NAME}
   compose_db_upgrade_do rm -s -f ${DB_SERVICE}
 }
 
 postgresql_upgrade() {
   compose_db_upgrade_do rm -s -f db db-old app
-  compose_db_upgrade_exec_with_wait db-old postgresql-container-make-dump.sh
-  compose_db_upgrade_run db-old postgresql-container-move-data.sh
-  compose_db_upgrade_exec_with_wait db postgresql-container-restore-dump.sh
+  compose_db_upgrade_exec_with_wait db-old make-dump.sh
+  compose_db_upgrade_run db-old move-data.sh
+  compose_db_upgrade_exec_with_wait db restore-dump.sh
 }
 
 app_upgrade() {
@@ -46,7 +46,7 @@ app_upgrade() {
 
 postgresql_upgrade_restore_backup() {
   compose_db_upgrade_do rm -s -f db db-old app
-  compose_db_upgrade_run db-old postgresql-container-restore-data.sh
+  compose_db_upgrade_run db-old restore-data.sh
 }
 
 app_and_postgresql_upgrade() {
