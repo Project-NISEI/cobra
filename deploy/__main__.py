9a3bda61b7c4bd3d90bc88b77cf0ecf8aa609f3f
haradan <haradan@users.noreply.github.com>
2022-11-04 18:28:33 +0000

    Use pulumi stack output to connect to droplet

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -1,7 +1,6 @@
 import pulumi
 import pulumi_digitalocean as do
 import pulumi_tls as tls
-import os
 
 config = pulumi.Config()
 
@@ -20,26 +19,5 @@ droplet = do.Droplet(
     ssh_keys=[ssh_key.fingerprint])
 
 
-private_key_filename = "id_cobra_rsa_{}".format(pulumi.get_stack())
-ssh_script_filename = "ssh-cobra-{}".format(pulumi.get_stack())
-
-
-def write_with_permissions(filename, permissions, body):
-    with os.fdopen(os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, permissions), "w") as file:
-        file.write(body)
-
-
-def write_private_key(key):
-    write_with_permissions(private_key_filename, 0o600, key)
-
-
-def write_connect_script(ip):
-    with open('ssh-template', 'r') as f:
-        ssh_script = f.read() \
-            .replace("%private_key_filename%", private_key_filename) \
-            .replace("%instance_ip%", ip)
-    write_with_permissions(ssh_script_filename, 0o700, ssh_script)
-
-
-private_key.private_key_openssh.apply(write_private_key)
-droplet.ipv4_address.apply(write_connect_script)
+pulumi.export("droplet_public_ip", droplet.ipv4_address)
+pulumi.export("private_key_openssh", private_key.private_key_openssh)
