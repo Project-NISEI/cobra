4cd0a26ceaed71c1e308d6521d8d21e320993ca5
haradan <haradan@users.noreply.github.com>
2022-11-05 22:33:47 +0000

    Fix marking rails secret key as secret for Pulumi

--- deploy/rails_secret_key_base.py
+++ deploy/rails_secret_key_base.py
@@ -10,7 +10,7 @@ class RailsSecretKeyBaseProvider(ResourceProvider):
         resource_id = binascii.b2a_hex(os.urandom(16)).decode("utf-8")
         key = binascii.b2a_hex(os.urandom(64)).decode("utf-8")
         return CreateResult(id_=resource_id, outs={
-            'result': Output.secret(key)
+            'result': key
         })
 
 
@@ -18,4 +18,9 @@ class RailsSecretKeyBase(Resource):
     result: Output[str]
 
     def __init__(self, name: str, opts: Optional[ResourceOptions] = None):
+        if opts is None:
+            opts = ResourceOptions()
+        if opts.additional_secret_outputs is None:
+            opts.additional_secret_outputs = []
+        opts.additional_secret_outputs.append('result')
         super().__init__(RailsSecretKeyBaseProvider(), name, {'result': None}, opts)
