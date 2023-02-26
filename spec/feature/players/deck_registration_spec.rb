3900e1c4bdda373ca9167dfa2c2c4e6e43d987ce
haradan <haradan@users.noreply.github.com>
2023-02-26 23:30:55 +0000

    Add flag for NRDB deck registration to UI

---
+++ spec/feature/players/deck_registration_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'registering a deck from NetrunnerDB' do
+  let(:organiser) { create(:user) }
+  let(:player) { create(:user) }
+  before do
+    sign_in organiser
+    visit new_tournament_path
+    fill_in 'Tournament name', with: 'Test Tournament'
+    check :tournament_self_registration
+    check :tournament_nrdb_deck_registration
+    click_button 'Create'
+  end
+
+  it 'registers a player with no deck' do
+    expect do
+      register_player
+    end.to change(Player, :count).by(1)
+    expect(page.current_path).to eq(tournament_path(Tournament.last))
+  end
+
+  def register_player
+    sign_in player
+    visit tournament_path(Tournament.last)
+    click_button 'Register'
+  end
+
+end
