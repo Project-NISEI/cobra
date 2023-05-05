3894dd86fe9a1747dfc0f8fbd7d6ab650851a496
haradan <haradan@users.noreply.github.com>
2023-05-05 22:47:37 +0100

    Add newlines to config/spring.rb

--- config/spring.rb
+++ config/spring.rb
@@ -1,9 +1,11 @@
+
 Spring.watch(
   ".ruby-version",
   ".rbenv-vars",
   "tmp/restart.txt",
   "tmp/caching-dev.txt"
 )
+
 Spring.after_fork do
   if ENV['DEBUGGER_STORED_RUBYLIB']
     ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
