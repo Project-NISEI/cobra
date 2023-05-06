7f3d175b17f218c8c73801d5837d1ccc7b09ba27
haradan <haradan@users.noreply.github.com>
2023-05-07 00:28:16 +0100

    Take Flipper.enable calls out of config/environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,8 +55,4 @@ Rails.application.configure do
   config.abr_host = 'https://alwaysberunning.net'
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.after_initialize do
-    Flipper.enable :nrdb_deck_registration
-  end
 end
