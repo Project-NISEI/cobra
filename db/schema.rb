32bd21b309c08c41a5b7e33d3781f4db946012ba
haradan <haradan@users.noreply.github.com>
2023-08-09 23:54:06 +0100

    Add pronouns in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_195151) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_225321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_195151) do
     t.boolean "consent_data_sharing"
     t.boolean "consent_streaming"
     t.boolean "consent_deck_sharing_with_to"
+    t.string "pronouns"
     t.index ["tournament_id"], name: "index_players_on_tournament_id"
     t.index ["user_id"], name: "index_players_on_user_id"
   end
