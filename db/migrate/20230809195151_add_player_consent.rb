f362ecf865836f3dc79acd2b957ac18466e75e6d
haradan <haradan@users.noreply.github.com>
2023-08-09 21:56:02 +0100

    Remove open cut consent flags

--- db/migrate/20230809195151_add_player_consent.rb
+++ db/migrate/20230809195151_add_player_consent.rb
@@ -2,8 +2,6 @@ class AddPlayerConsent < ActiveRecord::Migration[7.0]
   def change
     add_column :players, :consent_data_sharing, :boolean
     add_column :players, :consent_deck_sharing_with_to, :boolean
-    add_column :players, :consent_deck_sharing_with_opponent, :boolean
-    add_column :players, :consent_deck_sharing_with_public_if_in_cut, :boolean
     add_column :players, :consent_streaming, :boolean
   end
 end
