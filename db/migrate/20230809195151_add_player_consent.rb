d1489fd931b60b268d570a28c7339ccf62785329
haradan <haradan@users.noreply.github.com>
2023-08-09 21:56:40 +0100

    Reorder consent flags

--- db/migrate/20230809195151_add_player_consent.rb
+++ db/migrate/20230809195151_add_player_consent.rb
@@ -1,7 +1,7 @@
 class AddPlayerConsent < ActiveRecord::Migration[7.0]
   def change
     add_column :players, :consent_data_sharing, :boolean
-    add_column :players, :consent_deck_sharing_with_to, :boolean
     add_column :players, :consent_streaming, :boolean
+    add_column :players, :consent_deck_sharing_with_to, :boolean
   end
 end
