a8297d208b9e29a3f9af0f9acec595dd517bb949
haradan <haradan@users.noreply.github.com>
2023-08-19 21:58:20 +0100

    Add registration closed flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_194851) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_19_205733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -207,6 +207,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_194851) do
     t.boolean "all_players_unlocked", default: true
     t.boolean "any_player_unlocked", default: true
     t.boolean "open_list_cut", default: false
+    t.boolean "registration_closed"
     t.index ["user_id"], name: "index_tournaments_on_user_id"
   end
 
