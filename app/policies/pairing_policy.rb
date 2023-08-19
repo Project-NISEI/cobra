0d50daae9e493e2f9153113e005fa32a2f1631ad
haradan <haradan@users.noreply.github.com>
2023-08-20 00:31:53 +0100

    Use same logic in Pundit policy and view

--- app/policies/pairing_policy.rb
+++ app/policies/pairing_policy.rb
@@ -1,15 +1,5 @@
 class PairingPolicy < ApplicationPolicy
   def view_decks?
-    if record.tournament.user == user
-      return true
-    end
-    unless record.stage.double_elim?
-      return false
-    end
-    if @tournament.open_list_cut?
-      user == record.player1.user || user == record.player2.user
-    else
-      @tournament.public_list_cut?
-    end
+    record.cut_decks_visible_to(user)
   end
 end
