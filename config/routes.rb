c90d30c4a64e57aac7b17260bdd259fb864162d1
haradan <haradan@users.noreply.github.com>
2023-08-19 20:27:02 +0100

    Fix broken IDE refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
       get :download_decks, on: :collection
       patch :drop, on: :member
       patch :reinstate, on: :member
-      patch :close_registration, on: :member
-      patch :open_registration, on: :member
+      patch :lock_decks, on: :member
+      patch :unlock_decks, on: :member
       get :registration, on: :member
     end
     resources :rounds, only: [:index, :show, :create, :edit, :update, :destroy] do
