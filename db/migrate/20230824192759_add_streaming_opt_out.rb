dd55bdd2922bbf1ff4b85b9fb5e32ec580e1ffa1
haradan <haradan@users.noreply.github.com>
2023-09-12 23:39:46 +0100

    Fix migration script for streaming opt out

--- db/migrate/20230824192759_add_streaming_opt_out.rb
+++ db/migrate/20230824192759_add_streaming_opt_out.rb
@@ -1,4 +1,4 @@
-class AddStreamingConsent < ActiveRecord::Migration[7.0]
+class AddStreamingOptOut < ActiveRecord::Migration[7.0]
   def change
     add_column :tournaments, :allow_streaming_opt_out, :boolean
     add_column :players, :include_in_stream, :boolean, default: true
