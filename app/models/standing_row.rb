5a7d2f5ae3068d952b40e0891d44148771719a2c
haradan <haradan@users.noreply.github.com>
2023-11-01 00:15:41 +0000

    Stop looking up identities by name in standings

--- app/models/standing_row.rb
+++ app/models/standing_row.rb
@@ -4,19 +4,14 @@ class StandingRow < ApplicationRecord
 
   default_scope { order(position: :asc) }
 
-  delegate :name, :pronouns, :name_with_pronouns, :corp_identity, :runner_identity, :manual_seed, to: :player
+  delegate :name, :pronouns, :name_with_pronouns, :manual_seed, to: :player
 
   def corp_identity
-    identity(player.corp_identity)
+    player.corp_identity_object
   end
 
   def runner_identity
-    identity(player.runner_identity)
+    player.runner_identity_object
   end
 
-  private
-
-  def identity(id)
-    Identity.find_or_initialize_by(name: id)
-  end
 end
