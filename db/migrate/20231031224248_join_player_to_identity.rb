8d2d65883fe1350a09cf1464dcbb0fdcd62c7f95
haradan <haradan@users.noreply.github.com>
2023-11-08 23:33:45 +0000

    Improve player identity migration script

--- db/migrate/20231031224248_join_player_to_identity.rb
+++ db/migrate/20231031224248_join_player_to_identity.rb
@@ -2,9 +2,10 @@ class JoinPlayerToIdentity < ActiveRecord::Migration[7.0]
   def change
     add_reference :players, :corp_identity_ref, foreign_key: { to_table: :identities }
     add_reference :players, :runner_identity_ref, foreign_key: { to_table: :identities }
+    identities = Identity.all.index_by(&:name)
     Player.all.each do |player|
-      player.update(corp_identity_ref_id: Identity.find_by(name: player.corp_identity)&.id)
-      player.update(runner_identity_ref_id: Identity.find_by(name: player.runner_identity)&.id)
+      player.update_columns(corp_identity_ref_id: identities[player.corp_identity]&.id,
+                            runner_identity_ref_id: identities[player.runner_identity]&.id)
     end
   end
 end
