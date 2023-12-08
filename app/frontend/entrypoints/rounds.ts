4aad6859407a2dc4f9d3b4c594e0ae1fd7382d12
haradan <haradan@users.noreply.github.com>
2023-12-08 17:50:23 +0000

    Use turbolinks:load event in Svelte entrypoint

--- app/frontend/entrypoints/rounds.ts
+++ app/frontend/entrypoints/rounds.ts
@@ -1,11 +1,11 @@
 import Rounds from '../pairings/Rounds.svelte';
 
-const anchor = document.getElementById('rounds_anchor');
-const rounds = new Rounds({
-    target: anchor,
-    props: {
-        tournamentId: anchor.getAttribute('data-tournament')
-    }
+document.addEventListener("turbolinks:load", function () {
+    const anchor = document.getElementById('rounds_anchor');
+    new Rounds({
+        target: anchor,
+        props: {
+            tournamentId: anchor.getAttribute('data-tournament')
+        }
+    });
 });
-
-export default rounds;
