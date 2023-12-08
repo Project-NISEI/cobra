9237c7ea770dcd7c82d85271679ce9fe12245dce
haradan <haradan@users.noreply.github.com>
2023-12-08 20:29:51 +0000

    Don't re-render rounds when already rendered

--- app/frontend/entrypoints/rounds.ts
+++ app/frontend/entrypoints/rounds.ts
@@ -2,10 +2,12 @@ import Rounds from '../pairings/Rounds.svelte';
 
 document.addEventListener("turbolinks:load", function () {
     const anchor = document.getElementById('rounds_anchor');
-    new Rounds({
-        target: anchor,
-        props: {
-            tournamentId: anchor.getAttribute('data-tournament')
-        }
-    });
+    if (anchor && anchor.childNodes.length == 0) {
+        new Rounds({
+            target: anchor,
+            props: {
+                tournamentId: anchor.getAttribute('data-tournament')
+            }
+        });
+    }
 });
