a74a8afce8791e815d0b0be4059b3086b4fee01f
leukosaima <187358+leukosaima@users.noreply.github.com>
2024-02-06 22:55:19 -0500

    Build docker file, push to registry.  docker-override.init.yml to setup in one command (#263)
    * switch dockerfile base to alpine
    
    * support alpine image on apple silicon, dockerignore db and secrets
    
    * update readme

--- config/containers/app_cmd.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec unicorn -c config/containers/unicorn.rb -E $RAILS_ENV;
