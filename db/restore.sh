5900ce63fde2491bd6f6950f599b482b954ff97a
haradan <haradan@users.noreply.github.com>
2024-07-19 00:05:17 +0100

    Fix db/restore.sh script

--- db/restore.sh
+++ db/restore.sh
@@ -1,1 +1,7 @@
-cat ~/restore.sql | docker exec -i cobra_db_1 psql -U postgres
+#!/bin/sh
+
+RESTORE_FILE=$1
+
+# Make a database backup created by backup.sh
+
+gunzip "$RESTORE_FILE" -c | docker exec -i cobra-db-1 psql -U postgres
