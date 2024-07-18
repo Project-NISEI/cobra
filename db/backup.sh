d224b6ca0739dd9eec9e70d46fe51e52980a6142
haradan <haradan@users.noreply.github.com>
2024-07-19 00:06:06 +0100

    Use new docker compose command in db/backup.sh

--- db/backup.sh
+++ db/backup.sh
@@ -4,7 +4,7 @@ OUT_DIR=$1
 
 # Make a database backup in the specified $OUT_DIR
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml \
+docker compose -f docker-compose.yml -f docker-compose.prod.yml \
   exec db pg_dumpall -c -U postgres \
   | gzip -9 \
   > "${OUT_DIR}"/cobra-db-dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql.gz
