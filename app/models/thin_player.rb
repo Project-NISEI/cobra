915e7826fa211638b0b949078ea7ffeeb333f379
Jason Gessner <jason@multiply.org>
2024-11-16 15:54:39 +0000

    In progress ThinPlayer work.

--- app/models/thin_player.rb
+++ app/models/thin_player.rb
@@ -1,10 +1,19 @@
 class ThinPlayer
-  def initialize(id, name, opponents, points)
+  def initialize(id, name)
     @id = id
     @name = name
-    @opponents = opponents
-    @points = points
+    @opponents = []
+    @points = 0
   end
 
   attr_accessor :id, :name, :opponents, :points
+
+  def ==(other)
+    @id == other.id
+  end
+  alias eql? ==
+
+  def hash
+    [self.class, @id].hash
+  end
 end
