28fa8741907e84871cc6ffe25c22262e088d5cbd
Jason Gessner <jason@multiply.org>
2025-01-19 03:18:46 +0000

    Add test for changing tournament swiss settings.

--- spec/requests/tournaments_controller_spec.rb
+++ spec/requests/tournaments_controller_spec.rb
@@ -2,6 +2,10 @@
 
 RSpec.describe TournamentsController do
   let(:tournament) { create(:tournament, name: 'My Tournament') }
+  let(:player1) { create(:player, tournament:) }
+  let(:player2) { create(:player, tournament:) }
+  let(:player3) { create(:player, tournament:) }
+  let(:player4) { create(:player, tournament:) }
 
   describe '#save_json' do
     before do
@@ -35,6 +39,35 @@ RSpec.describe TournamentsController do
       post cut_tournament_path(tournament), params: { number: 8 }
 
       expect(tournament).to have_received(:cut_to!).with(:double_elim, 8)
+    end
+  end
+
+  describe '#change_swiss_format' do
+    before do
+      allow(Tournament).to receive(:find)
+        .with(tournament.to_param)
+        .and_return(tournament)
+    end
+
+    it 'changes swiss format when stage has no pairings' do
+      sign_in tournament.user
+
+      patch tournament_path(tournament), params: { tournament: { swiss_format: 'single_sided' } }
+
+      expect(tournament.single_sided?).to be(true)
+    end
+
+    it 'will not change swiss format when stage has pairings' do
+      expect(tournament.swiss?).to be(true)
+      tournament.pair_new_round!
+      sign_in tournament.user
+
+      patch tournament_path(tournament), params: { tournament: { swiss_format: 'single_sided' } }
+
+      expect(flash[:alert]).to eq("Can't change Swiss format when rounds exist.")
+
+      # Format is unchanged
+      expect(tournament.swiss?).to be(true)
     end
   end
 end
