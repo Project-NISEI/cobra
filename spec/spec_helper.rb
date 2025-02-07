c73aba128b490b424f6186c16a7e751c469b3007
plural <jason@multiply.org>
2025-02-07 00:26:29 -0600

    Collect code coverage for rspec tests. (#398)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'simplecov-cobertura'
+SimpleCov.coverage_dir 'coverage/spec'
+SimpleCov.enable_coverage :branch
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
+SimpleCov.start do
+  add_filter 'spec/'
+  add_filter 'test/'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Resources', 'app/resources'
+  add_group 'Libraries', 'lib'
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
