f9fb6050d3ff25abd9a7709030c835e2d944a444
talhaahsan <stwyde@gmail.com>
2025-06-02 03:33:18 +0000

    lint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ require 'redcarpet'
 module ApplicationHelper
 
   class ResponsiveImgRenderer < Redcarpet::Render::HTML 
-    def image(link, _title, alt_text) 
+    def image(link, _title, alt_text)
       %(<img class="img-fluid" src=#{link} alt=#{alt_text}>)
     end
   end
