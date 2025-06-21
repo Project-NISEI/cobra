1a3d36e2b1074054a4d73c0b7af5524ac37b8f51
haradan <haradan@users.noreply.github.com>
2025-06-22 00:31:15 +0100

    Comment MSW setup

--- app/frontend/msw/server.ts
+++ app/frontend/msw/server.ts
@@ -1,3 +1,9 @@
 import { setupServer, SetupServerApi } from "msw/node";
 
+/**
+ * This creates a server for Mock Service Worker. Vitest is configured to manage
+ * this so that it will be available for any test named in the format
+ * MyName.msw-test.ts. This can be imported in those tests to fake server side
+ * behaviour.
+ */
 export const server: SetupServerApi = setupServer();
