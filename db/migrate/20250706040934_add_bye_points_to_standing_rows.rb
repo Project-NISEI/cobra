0d5ce37e3fb2e753bc7318da39652b4326a45a50
Jason Gessner <jason@multiply.org>
2025-07-06 16:57:38 +0000

    Set bye_points as part of the migration.

--- db/migrate/20250706040934_add_bye_points_to_standing_rows.rb
+++ db/migrate/20250706040934_add_bye_points_to_standing_rows.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class AddByePointsToStandingRows < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :standing_rows, :bye_points, :integer, default: 0
+    connection.exec_update <<-SQL
+      UPDATE standing_rows
+      SET bye_points = points - (corp_points + runner_points)
+      WHERE points <> (corp_points + runner_points)
+    SQL
+  end
+
+  def down
+    remove_column :standing_rows, :bye_points
   end
 end
